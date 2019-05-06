a8ec17936ccd888d168ffce184eb9776ee761edc
liboming <liboming@smg.cn>
2019-05-06 18:31:07 +0800

    update helper array

--- frontend/controllers/HelperController.php
+++ frontend/controllers/HelperController.php
@@ -11,7 +11,9 @@ use frontend\models\Country;
 class HelperController extends Controller {
 
 	public function actionHtml(){
-		echo Html::encode('Test > test');	
+		// $this->layout = false;
+		// echo Html::encode('Test > test');	
+		return $this->render('html');
 	}
 
 	public function actionArray(){
---
+++ frontend/views/helper/html.php
@@ -0,0 +1,87 @@
+<?php
+
+
+use \yii\helpers\Html;
+
+echo Html::tag('p', Html::encode('Henry &nbsp; Lee'), ['class' => 'username']);
+
+# css
+echo Html::BeginTag('div', ['style' => ['height' => '50px']]);
+$options = ['class' => 'btn btn-default'];
+$type = 'success';
+if ($type === 'success') {
+	Html::removeCssClass($options, 'btn-default');
+	Html::addCssClass($options, 'btn-success');
+}
+echo Html::tag('div', 'Pwede na', $options);
+Html::addCssClass($options, ['btn-danger', 'btn-lg']);
+echo Html::tag('div', 'Save', $options);
+echo Html::endTag('div');
+
+# style
+echo Html::BeginTag('div',  ['style' => ['height' => '50px']]);
+$options = ['style' => ['width' => '100px', 'height' => '100px', 'background-color' => 'purple']];
+// Html::addCssStyle($options, 'height:200px; position:absolute;');
+Html::removeCssStyle($options, ['width', 'hright']);
+echo Html::tag('div', 'Style', $options);
+echo Html::endTag('div');
+
+# encode decode
+echo Html::BeginTag('div',['style' => ['height' => '50px']]);
+$content = Html::encode('上海女记者合唱团祝记   者朋友们记者节快乐');
+echo $content;
+$decode = Html::decode($content);
+echo $decode;
+echo Html::endTag('div');
+
+# form
+
+echo Html::BeginTag('div',['style' => ['height' => '500px']]);
+echo Html::beginForm(['order/update', 'id' => 10], 'post', ['enctype' => 'multipart/form-data']);
+
+# input
+echo Html::BeginTag('div');
+echo Html::input('text', 'username', 'Bluebird89', ['class' => 'username']);
+echo Html::activeInput('text', new \frontend\models\User(), 'firstName', ['class' => 'username']);
+echo Html::getInputName(new \frontend\models\User(), 'name');
+echo Html::getAttributeValue(new \frontend\models\User(), 'name');
+echo Html::endTag('div');
+
+# radio
+echo Html::BeginTag('div');
+echo Html::radio('agree', true,  ['label' => 'I  agree']);
+echo Html::activeRadio(new \frontend\models\User(), 'username', ['class' => 'agreement']);
+echo Html::endTag('div');
+
+# checkbox
+echo Html::BeginTag('div');
+echo Html::checkbox('agree', true,  ['label' => 'I  agree']);
+echo Html::activeCheckbox(new \frontend\models\User(), 'username', ['class' => 'agreement']);
+echo Html::endTag('div');
+
+# dropdwonlist
+echo Html::BeginTag('div');
+echo Html::dropDownList('list', 2,  ['label' => 'I  agree']);
+echo Html::activedropDownList(new \frontend\models\User(), 'id', ['class' => 'agreement']);
+echo Html::endTag('div');
+
+# listbox
+echo Html::BeginTag('div');
+echo Html::listBox('agree', true,  ['label' => 'I  agree']);
+echo Html::activeListBox(new \frontend\models\User(), 'username', ['class' => 'agreement']);
+echo Html::endTag('div');
+
+
+echo Html::label('User name', 'username', ['class' => 'label username']);
+echo Html::activelabel(new \frontend\models\User(), 'firstName', ['class' => 'label username']);
+
+# button
+# active:依据指定模型与属性获取数据
+echo Html::BeginTag('div');
+echo Html::button('Press me!', ['class' => 'teaser']);
+echo Html::button('Submit', ['class' => 'submit']);
+echo Html::button('Reset', ['class' => 'reset']);
+echo Html::endTag('div');
+
+echo Html::endForm();
+echo Html::endTag('div');
